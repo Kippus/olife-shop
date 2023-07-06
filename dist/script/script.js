e29f7f59f0af7331c09dda5c94c0005d829f5adf
Kippus <vashenkov.ip@gmail.com>
2023-07-06 13:29:24 +0500

    one

---
+++ dist/script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+$(document).ready(function(){
+    $('.modal').modal();
+});
